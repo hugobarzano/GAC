522b5b52b58a689192cef91776c8453097b08032
hugobarzano <cesarhugo.barzano@bbva.com>
2018-08-31 19:58:16 +0200

    end doc

--- Trabajo2_CesarHugoBarzanoCruz/TestCase_4.sh
+++ Trabajo2_CesarHugoBarzanoCruz/TestCase_4.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export GENERATOR_CONFIG=./input/GAC_GENERATOR_CONFIG_4.json
-python generadorP2.py -t gac_models
+python generadorP2.py -t gac_models --where "model_no = '1'"
---
+++ Trabajo2_CesarHugoBarzanoCruz/TestCase_6.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export GENERATOR_CONFIG=./input/GAC_GENERATOR_CONFIG_error.json
+python generadorP2.py -t gac_models
